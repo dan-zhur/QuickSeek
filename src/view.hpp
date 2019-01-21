92413ffcb83fe46afc789203db3a11f26f5012a0
noolo113 <noolo113@gmail.com>
2019-01-22 01:07:45 +0300

    code formatting

--- src/view.hpp
+++ src/view.hpp
@@ -7,21 +7,21 @@
 
 class View : public ViewBase {
 public:
-	//special members
+	// special members
 	~View() override = default;
 	//------------------------------
 
-	//overriding functions
+	// overriding functions
 	void ShowMainWindow() override;
 	void AddPathToList(const std::string& path) override;
 	void ClearList() override;
-	void ShowScanningWindow(const std::string& diskLetter) override;
-	void SetScanningDiskLetter(const std::string& diskLetter) override;
+	void ShowScanningWindow(const std::string& disk_letter) override;
+	void SetScanningDiskLetter(const std::string& disk_letter) override;
 	void CloseScanningWindow() override;
-	void NotifySearchButtonClicked(const std::string& whatToSearch) override;
+	void NotifySearchButtonClicked(const std::string& what_to_search) override;
 	//------------------------------
 private:
-	//member variables
+	// member variables
 	MainWindow _main_window{ this };
 	ScanWindow _scan_window;
 	//------------------------------
